c615d45085de304f13cae2207d74fc934b0d468d
Gabriel <ajjgabriel@gmail.com>
2018-07-10 00:49:10 +0800

    first blood

--- server/helpers/constants.js
+++ server/helpers/constants.js
@@ -2,6 +2,5 @@
  * Created by gaang on 18/4/18.
  */
 module.exports = {
-   sharedSecret: "$CEACDbytDLncGt0TgRb$@tMz1-yJl/fug/Z7S}w",
-   apikey: "ANXU9MJV351ZQ1B56YUG21fuZ9zFuazEjOj5AH_F2MjsuQiEY"
+   
 };
